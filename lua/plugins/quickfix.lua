b87aab6097b992ed7372b79823cce7780e38cfe0
HemendraSinghShekhawat <hemustudys@gmail.com>
2025-09-02 16:28:10 +0530

    moved from qf_helper.nvim(now depricated) to quicker.nvim

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -7,7 +7,13 @@ return {
 		end,
 	},
 	{
-		"stevearc/qf_helper.nvim",
+		"stevearc/quicker.nvim",
+		event = "FileType qf",
+		---@module "quicker"
+		---@type quicker.SetupOptions
 		opts = {},
+		config = function()
+			require("quicker").setup()
+		end,
 	},
 }
